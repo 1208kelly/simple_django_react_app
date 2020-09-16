1491e5559c4df3df4ceb5b06b1c4d0bf40a33efb
1208kelly <1208kelly@gmail.com>
2020-09-16 21:04:51 +0000

    Adding api overview item

--- api/views.py
+++ api/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
+
 # Create your views here.
 
 
+
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+		'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)
