d5f51190f00b4ee3e65ad2da67a0fb41e7d9eda2
1208kelly <1208kelly@gmail.com>
2020-09-19 16:54:56 +0000

    Fixed error when running app.js in react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-class App extends React.component {
+class App extends React.Component {
     render(){
         return(
             <div className="container">
