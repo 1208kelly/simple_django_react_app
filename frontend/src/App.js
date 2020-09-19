c5e01cbc56e013090a1f9a054ccbd197eb0ab29d
1208kelly <1208kelly@gmail.com>
2020-09-19 16:51:48 +0000

    Editing app.js in react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.component {
+    render(){
+        return(
+            <div className="container">
+                <div id="task-container">
+                    <div id="form-wrapper">
+                        <form id="form">
+                            <div className="flex-wrapper">
+                                <div style={{flex: 6}}>
+                                    <input className="form-control" id="title" type="text" name="title" placeholder="Add Task"></input>
+                                </div>
+                            </div>
+
+                            <div style={{flex: 1}}>
+                                <input id="submit" className="btn btn-warning" type="submit" name="Add"></input>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="list-wrapper">
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
