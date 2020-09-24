81070367bae31d8bce2826ebb4f4a254bc7c5eab
1208kelly <1208kelly@gmail.com>
2020-09-24 19:02:09 +0000

    Modified static files in settings.py and added urls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -81,13 +81,12 @@ class App extends React.Component {
 
         var url = 'http://127.0.0.1:8000/api/task-create/'
 
-        if(this.state.editing == true){
+        if(this.state.editing === true){
             url = `http://127.0.0.1:8000/api/task-update/${ this.state.activeItem.id}/`
             this.setState({
                 editing:false
             })
         }
-
 
         fetch(url, {
             method:'POST',
@@ -179,7 +178,7 @@ class App extends React.Component {
                             return(
                                 <div key={index} className="task-wrapper flex-wrapper">
                                     <div onClick={() => self.strikeUnstrike(task)} style={{flex: 7}}>
-                                        {task.completed == false ? (
+                                        {task.completed === false ? (
                                             <span>{task.title}</span>
 
                                         ) : (
