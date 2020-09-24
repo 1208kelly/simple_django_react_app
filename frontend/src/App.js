10b89c0d62ba003a66cd4a18d1c41815967be18e
1208kelly <1208kelly@gmail.com>
2020-09-24 18:08:19 +0000

    added event handler to set form value being clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,17 +45,13 @@ class App extends React.Component {
     fetchTasks(){
         console.log('Fetching...');
 
-        fetch('http://127.0.0.1:8000/api/task-list/', {
-            method: "POST",
-            headers: {
-                "Content-Type": "text/plain"
-            },
-        }).then(response => response.json())
+        fetch('http://127.0.0.1:8000/api/task-list/')
+        .then(response => response.json())
         .then(data =>
             this.setState({
                 todoList: data
             })
-        );
+        )
     }
 
 
@@ -68,7 +64,7 @@ class App extends React.Component {
         this.setState({
             activeItem:{
                 ...this.state.activeItem,
-                title:value
+                title: value
             }
         })
     }
@@ -78,9 +74,17 @@ class App extends React.Component {
         e.preventDefault()
         console.log('ITEM:', this.state.activeItem)
 
-        var csrftoken = this.getCookie('csrftoken');
+        var csrftoken = this.getCookie('csrftoken')
 
         var url = 'http://127.0.0.1:8000/api/task-create/'
+
+        if(this.state.editing == true){
+            url = `http://127.0.0.1:8000/api/task-update/${ this.state.activeItem.id}/`
+            this.setState({
+                editing:false
+            })
+        }
+
 
         fetch(url, {
             method:'POST',
@@ -98,19 +102,23 @@ class App extends React.Component {
                     completed: false,
                 }
             })
-        }).catch(function(err){
-            if (err)
-                return res.send(err);
+        }).catch(function(error){
+            console.log('ERROR:', error)
+        })
 
-            res.json({
-                message: 'Read file!'
-        });
+    }
 
-    },
+    startEdit(task){
+        this.setState({
+        activeItem:task,
+        editing:true,
+        })
+    }
 
 
     render(){
         var tasks = this.state.todoList;
+        var self = this
         return(
             <div className="container">
                 <div id="task-container">
@@ -135,7 +143,7 @@ class App extends React.Component {
                                         <span>{task.title}</span>
                                     </div>
                                     <div style={{flex: 1}}>
-                                        <button className="btn btn-sm btn-outline-info">Edit</button>
+                                        <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
                                     </div>
                                     <div style={{flex: 1}}>
                                         <button className="btn btn-sm btn-outline-dark delete">-</button>
